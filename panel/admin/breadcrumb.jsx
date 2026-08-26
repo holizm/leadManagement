067faea8a23645eee61c99cba6d9371ad46ebc79
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:02 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='personSearch'
+    name='leadManagement'
+    title='leadManagementLeadManagement'
+    {...props}
+/>
